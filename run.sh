981fe9079ed29285a537112cd73e549b5df31ac3
Savithru Lokanath <savithru@icloud.com>
2016-04-21 13:57:03 -0600

    Automated Install Process

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo echo "127.0.0.2	ralphie.com" >> /etc/hosts
+sudo iptables -t nat -A OUTPUT -d 127.0.0.2 -p tcp --dport 80 -j REDIRECT --to-port 8080
+ant; ./floodlight.sh
+
+echo "Access the controller: http://ralphie.com/ui/index.html"
